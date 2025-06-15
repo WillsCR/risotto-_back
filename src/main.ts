ae5cc15dd01f24da23f55af0ed294aa875a14940
javierm-0 <javier.moraga@alumnos.ucn.cl>
2025-06-14 21:20:00 -0400

    Cambios leves
    incluir patch en CORS
    ajuste de tipo en update caso

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:5173'],
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
   })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
--- src/simulation/simulation.controller.ts
+++ src/simulation/simulation.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, UsePipes, ValidationPipe, UseGuards, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, ValidationPipe, UseGuards, BadRequestException, Patch } from '@nestjs/common';
 import { SimulationService } from './simulation.service';
 import { Case } from './schema/case.scheme';
-import { CreateCaseDto } from "./dto/case.dto";
 import { Param } from '@nestjs/common';
 import { CaseDto } from './dto/case2.dto';
 import { GoogleAuthGuard } from 'src/auth/authGuard';
@@ -31,8 +30,11 @@ export class SimulationController{
     return this.simulationService.getCaseById(id);    
     }
     
-    @Post('/case/update/:id')
-    async updateCase(@Param('id') id: string, @Body() updateCaseDto: CreateCaseDto): Promise<Case> {
+    @Patch('/case/update/:id')
+    async updateCase(@Param('id') id: string, @Body() updateCaseDto: CaseDto): Promise<Case> {
+        if (!isValidObjectId(id)) {
+            throw new BadRequestException('ID inválido');
+        }
         return this.simulationService.updateCase(id, updateCaseDto);
     }
 
