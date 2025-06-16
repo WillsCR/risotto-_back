2edfd95d72be3cd1431586b5740ce7f701faf0fc
WillsCR <102868946+WillsCR@users.noreply.github.com>
2025-06-16 18:11:35 -0400

    cambios para vm

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhot:5173'],
     methods: 'GET,POST,PUT,DELETE',
   })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000); 
 }
 bootstrap();
